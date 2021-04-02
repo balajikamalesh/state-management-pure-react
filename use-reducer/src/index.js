99906da03351350b2548145181197b48a3c237b7
Balaji <abalajikamalesh@gmail.com>
2021-04-02 13:49:04 +0530

    Intitial implementation using useState

---
+++ use-reducer/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Application from './Application';
+import './styles.css';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<Application />, rootElement);
